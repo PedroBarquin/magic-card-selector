68e8ccb218c9b1cc2686ab7670700cb5624c25ff
Pedro Barquín <pbarquin@uxland.cat>
2022-08-14 11:30:26 +0200

    display list of cards

---
+++ src/infraestructure/http/http.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class HttpClient {
+   
+    get(url: string, config?: any) {
+        return axios.get(url, config);
+    }
+    post(url: string, data?: any, config?: any) {
+        return axios.post(url, data, config);
+    }
+
+    put(url: string, data?: any, config?: any) {
+        return axios.put(url, data, config);
+    }
+
+    patch(url: string, data?: any, config?: any) {
+         return axios.patch(url, data, config);
+    }
+}
+
+export const httpClient = new HttpClient();
